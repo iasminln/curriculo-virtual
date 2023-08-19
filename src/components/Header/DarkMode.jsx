a780d3ae674cb75a45f25d3a7359dbb008d8060a
Iasmin <iasln@outlook.com>
2023-08-19 04:35:01 -0300

    added auto dark mode

--- src/components/Header/DarkMode.jsx
+++ src/components/Header/DarkMode.jsx
@@ -19,7 +19,7 @@ const DarkMode = () => {
       </button>
       }
     </div>
-  )
+  ) 
 }
 
 export default DarkMode;
--- src/userContext.jsx
+++ src/userContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 
 export const UserContext = createContext()
@@ -6,13 +6,20 @@ export const UserContext = createContext()
 export const UserStorage = ({ children }) => {
   const [darkAtivo, setDarkAtivo] = useState(false);
 
+  useEffect(() => {
+    const viewMode = window.localStorage.getItem('portfolioViewMode')
+    if (viewMode) handleClick(viewMode)
+  }, [])
+
   const handleClick = (tipo) => {
     if (tipo === 'dark') {
       document.body.classList.add('darkmode')
+      window.localStorage.setItem('portfolioViewMode', 'dark')
       setDarkAtivo(true)
     } else {
       document.body.classList.remove('darkmode')
       setDarkAtivo(false)
+      window.localStorage.setItem('portfolioViewMode', 'light')
     }
   }
 
