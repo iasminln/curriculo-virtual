ff4651202d3775e5172a7cfcfb6ebd499adb7147
Iasmin <iasln@outlook.com>
2023-08-25 02:48:56 -0300

    fixed projetos section

--- src/components/Projetos/ItemProjeto.jsx
+++ src/components/Projetos/ItemProjeto.jsx
@@ -1,32 +1,55 @@
-import React, { useState } from 'react';
-import useMedia from '../../Hooks/useMedia';
-import { IconDireita } from './Icons'
-import { IconEsquerda } from './Icons';
-import ImageWebp from '../Util/ImageWebp';
+import React, { useState, useContext } from "react";
+import { IconDireita } from "./Icons";
+import { IconEsquerda } from "./Icons";
+import ImageWebp from "../Util/ImageWebp";
+import { UserContext } from "../../userContext";
 
 const ItemProjeto = ({ img, imgWebp, title, hover, height, width }) => {
-  const isMobile = useMedia('(max-width: 47.5rem)')
-  const [visibleImg, setVisibleImg] = useState(true)
-
+  const [visibleImg, setVisibleImg] = useState(true);
+  const { darkAtivo } = useContext(UserContext)
+  const colorNavIcons = darkAtivo ? '#FAEBEB' : '#210808'
 
   return (
-    <section className='item-projeto'>
-      <div className='box'>
-        {isMobile ? visibleImg ? <ImageWebp src={img} srcWebp={imgWebp} alt={title} width={width} height={height} /> : null : <ImageWebp src={img} srcWebp={imgWebp} alt={title} />}
-        {isMobile ? !visibleImg ? <div className='text-hover'>{hover}</div> : null : <div className='text-hover'>{hover}</div>}
-
+    <div className="item-projeto" aria-label={`Projeto: ${title}`}>
+      <div className="box">
+        {visibleImg ? (
+          <ImageWebp
+            src={img}
+            srcWebp={imgWebp}
+            alt={title}
+            width={width}
+            height={height}
+          />
+        ) : null}
+        {!visibleImg ? <div className="text-hover">{hover}</div> : null}
       </div>
 
-      {isMobile &&
-        <div className='container-btn-slide'>
-          <button className='btn-slide' disabled={visibleImg ? true : false} onClick={() => { setVisibleImg(true) }}><IconEsquerda /></button>
-          <button className='btn-slide' disabled={!visibleImg ? true : false} onClick={() => { setVisibleImg(false) }}><IconDireita /></button>
-        </div>
-      }
-      <p className='title'>{title}</p>
+      <div className="container-btn-slide">
+        <button
+          className="btn-slide"
+          aria-label="Clique para ver a imagem."
+          disabled={visibleImg ? true : false}
+          onClick={() => {
+            setVisibleImg(true);
+          }}
+        >
+          <IconEsquerda color={colorNavIcons} />
+        </button>
+        <button
+          className="btn-slide"
+          aria-label="Clique para ver o box de informações."
+          disabled={!visibleImg ? true : false}
+          onClick={() => {
+            setVisibleImg(false);
+          }}
+        >
+          <IconDireita color={colorNavIcons} />
+        </button>
+      </div>
 
-    </section>
-  )
-}
+      <p className="title">{title}</p>
+    </div>
+  );
+};
 
 export default ItemProjeto;
